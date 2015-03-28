5e725a5b9c87a9c32f1e9df3d495be88b348a16e
Alexander Nelzin <asnelzin@gmail.com>
2015-03-28 14:13:00 +0300

    Add first task

---
+++ FirstTask.playground/section-1.swift
@@ -0,0 +1,47 @@
+//func natural(a: Int, b: Int, n: Int) {
+//    
+//    for i in a..b {
+//        
+//    }
+//}
+//
+//natural(1, 1000, 1)
+import Foundation
+
+infix operator ^^ { }
+func ^^ (radix: Int, power: Int) -> Int {
+    return Int(pow(Double(radix), Double(power)))
+}
+
+func numbersWithNSevens(a: Int, b:Int, n:Int) {
+    var sevensCount: Int
+    for number in a...b {
+        sevensCount = String(number).componentsSeparatedByString("7").count - 1
+        if sevensCount == n {
+            println(number)
+        }
+    }
+}
+
+
+numbersWithNSevens(1, 10, 1)
+
+
+
+func reverseNumber(num: Int) -> Int {
+    if (num < 100) || (num > 999) {
+        return -1
+    }
+    var result: Int = 0
+    var current: Int = num
+    for var power = 2; power >= 0; --power  {
+        result = result + (current % 10) * (10^^power)
+        current = current / 10
+    }
+    return result
+}
+
+reverseNumber(123)
+
+
+
